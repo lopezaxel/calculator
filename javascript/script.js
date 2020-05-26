2c26f9daff64e13f66ad6dc1ccff59df3b0337e2
Axel Lopez <lopezaxel@protonmail.com>
2020-05-26 12:09:27 -0300

    Added basic calculator

--- javascript/script.js
+++ javascript/script.js
@@ -20,15 +20,41 @@ function operate(num1, num2, operator) {
 
 function showNumbers() {
   const display = document.querySelector("#display p");
-  const buttons = document.querySelectorAll(".number");
-  const operators = document.querySelectorAll(".operator");
-  let displayValue = ""; 
+  const buttons = document.querySelectorAll("button");
+  let firstNum = ""; 
+  let secondNum = "";
+  let operator = "";
 
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-      display.textContent += button.textContent;
-      displayValue += button.textContent;
-      console.log(displayValue);
+      if(button.className == "number" && operator != "") {
+        display.textContent += button.textContent;
+        secondNum += button.textContent;
+        console.log("Second num:" + secondNum);
+      } else if(button.className == "number") {
+        display.textContent += button.textContent;
+        firstNum += button.textContent;
+        console.log("First num:" + firstNum);
+      } else if(button.className == "operator") {
+        display.textContent += button.textContent;
+        operator = button.textContent;
+        console.log("Operator:" + operator);
+      } else if(button.className == "equalBtn") {
+        switch (operator) {
+          case "+":
+            display.textContent = add(+firstNum, +secondNum);
+            break;
+          case "-":
+            display.textContent = subtract(+firstNum, +secondNum);
+            break;
+          case "*":
+            display.textContent = multiply(+firstNum, +secondNum);
+            break;
+          case "/":
+            display.textContent = divide(+firstNum, +secondNum);
+            break;
+        }
+      }
     });
   });
 
