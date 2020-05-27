98d86c679e65f6504b218ca44b61ca6113d0a8d4
Axel Lopez <lopezaxel@protonmail.com>
2020-05-27 10:48:40 -0300

    Changed calculator logic

--- javascript/script.js
+++ javascript/script.js
@@ -21,43 +21,57 @@ function operate(num1, num2, operator) {
 function showNumbers() {
   const display = document.querySelector("#display p");
   const buttons = document.querySelectorAll("button");
-  let firstNum = ""; 
-  let secondNum = "";
+  let displayValue = "";
+  let num = "";
   let operator = "";
+  let firstNum = "";
 
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-      if(button.className == "number" && operator != "") {
-        display.textContent += button.textContent;
-        secondNum += button.textContent;
-        console.log("Second num:" + secondNum);
-      } else if(button.className == "number") {
-        display.textContent += button.textContent;
-        firstNum += button.textContent;
-        console.log("First num:" + firstNum);
+      displayValue += button.textContent;
+      display.textContent += button.textContent;
+      console.log(displayValue);
+
+      if(button.className == "number") {
+        num += button.textContent;
       } else if(button.className == "operator") {
-        display.textContent += button.textContent;
+        firstNum = num;
         operator = button.textContent;
-        console.log("Operator:" + operator);
+        num = "";
+        console.log(`First num: ${firstNum} Operator ${operator}`);
       } else if(button.className == "equalBtn") {
-        switch (operator) {
-          case "+":
-            display.textContent = add(+firstNum, +secondNum);
-            break;
-          case "-":
-            display.textContent = subtract(+firstNum, +secondNum);
-            break;
-          case "*":
-            display.textContent = multiply(+firstNum, +secondNum);
-            break;
-          case "/":
-            display.textContent = divide(+firstNum, +secondNum);
-            break;
+        for(let i = 0; i < displayValue.length; i++) {
+          let multiplyOp = displayValue.indexOf("*");
+          if(multiplyOp != -1) {
+            let nextOperator = findOperator(displayValue, multiplyOp, +1);
+            console.log(`Operator ${multiplyOp} Index ${nextOperator}`);
+          } 
         }
       }
-    });
+    }); 
   });
+}
 
+function findOperator(string, index) {
+  let operators = /[/+/-/*//]/;
+  for(let i = index + 1; i < string.length; i++) {
+    if(operators.test(string[i])) {
+      return i;
+    }
+  }
+}
+
+function determineOperator(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(+num1, +num2);
+    case "-":
+      return subtract(+num1, +num2);
+    case "*":
+      return multiply(+num1, +num2);
+    case "/":
+      return divide(+num1, +num2);
+  }
 }
 
 showNumbers();
