8175aa8c0e1614f9405e494fb4a780727fd34b91
Axel Lopez <lopezaxel@protonmail.com>
2020-05-28 09:38:01 -0300

    Reformatted and shorted code

--- javascript/script.js
+++ javascript/script.js
@@ -15,7 +15,16 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, operator) {
-  return add(num1, num2);
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
 }
 
 function showNumbers() {
@@ -23,85 +32,47 @@ function showNumbers() {
   const buttons = document.querySelectorAll("button");
   let displayValue = [];
   let num = "";
-  let operator = "";
-  let firstNum = "";
   let result = "";
 
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-      // displayValue += button.textContent;
       display.textContent += button.textContent;
-      
+
       if(button.className == "number") {
         num += button.textContent;
       } else if(button.className == "operator") {
-        firstNum = num;
-        operator = button.textContent;
+        displayValue.push(num);
+        displayValue.push(button.textContent);
         num = "";
-        displayValue.push(firstNum);
-        displayValue.push(operator);
       } else if(button.className == "equalBtn") {
         displayValue.push(num);
         
-        for(let i = 0; i <= displayValue.length; i++) {
-          switch (displayValue[i]) {
-            case "+":
-              result = +displayValue[i - 1] + +displayValue[i + 1];
-              displayValue.splice(displayValue[i - 2], 3, result);
-              i = 0;
-              break;
-            case "-":
-              result = +displayValue[i - 1] - +displayValue[i + 1];
-              displayValue.splice(displayValue[i - 2], 3, result);
-              i = 0;
-              break;
-            case "*":
-              result = +displayValue[i - 1] * +displayValue[i + 1];
-              displayValue.splice(displayValue[i - 2], 3, result);
-              i = 0;
-              break;
-            case "/":
-              result = +displayValue[i - 1] / +displayValue[i + 1];
-              displayValue.splice(displayValue[i - 2], 3, result);
-              i = 0;
-              break;
-          }
+        for(let i = 0; i < displayValue.length; i++) {
+          calculate(displayValue, result, i)
+          i = 0;
+          display.textContent = displayValue;
           console.log(displayValue);
-          console.log('Result :' + result);
         }
       }
     }); 
   });
 }
 
-function findOperator(string, index, direction) {
-  let operators = /[=+-/*//]/;
-  if(direction == +1) {
-    for(let i = index + 1; i < string.length; i++) {
-      if(operators.test(string[i])) {
-        return i - 1;
-      }
-    }
-  } else if (direction == -1) {
-    for(let i = index - 1; i > 0; i--) {
-      if(operators.test(string[i])) {
-        return i;
-      }
-    }
-  }
-}
-
-function determineOperator(operator, num1, num2) {
-  switch (operator) {
+function calculate(calculation, result, i) {
+  switch (calculation[i]) {
     case "+":
-      return add(+num1, +num2);
+      result = operate(+calculation[i - 1], +calculation[i + 1], "+");
+      return calculation.splice(calculation[i - 2], 3, result);
     case "-":
-      return subtract(+num1, +num2);
+      result = operate(+calculation[i - 1], +calculation[i + 1], "-");
+      return calculation.splice(calculation[i - 2], 3, result);
     case "*":
-      return multiply(+num1, +num2);
+      result = operate(+calculation[i - 1], +calculation[i + 1], "*");
+      return calculation.splice(calculation[i - 2], 3, result);
     case "/":
-      return divide(+num1, +num2);
-  }
+      result = operate(+calculation[i - 1], +calculation[i + 1], "/");
+      return calculation.splice(calculation[i - 2], 3, result);
+    }
 }
 
 showNumbers();
